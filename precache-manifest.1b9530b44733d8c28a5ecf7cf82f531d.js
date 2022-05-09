8e5345ba123867092ca9169682a98a61f9c70551
agateblue <me+github@agate.blue>
2022-05-09 16:29:54 +0000

    Deploying to gh-pages from @ agateblue/demonology@651c2f0aa4865b91701b79c1bef6dd0b51ee2a9b 🚀

---
+++ precache-manifest.1b9530b44733d8c28a5ecf7cf82f531d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f8de7df530f568507d",
+    "url": "/css/app.fce91396.css"
+  },
+  {
+    "revision": "359e1b8191438fae5a69415ee5f7844c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f8de7df530f568507d",
+    "url": "/js/app.907118af.js"
+  },
+  {
+    "revision": "91cb5a57f967a027212a",
+    "url": "/js/chunk-vendors.c651ea0a.js"
+  },
+  {
+    "revision": "b0a544c243059010347f7dfc5492932b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
