03a2a38d6debd54d14e9e598130b23cdb5a2cda1
Agate <me@agate.blue>
2022-05-13 11:59:01 +0200

    Perf improvement by reducing NumberFormat instanciation

--- src/utils.js
+++ src/utils.js
@@ -4,12 +4,15 @@ export function renderMarkdown (source) {
   return marked.parse(source)
 }
 
+const NOTATIONS = {
+  compact: new Intl.NumberFormat('en-US', {notation: 'compact', minimumFractionDigits: 2})
+}
 export function formatNumber (n, notation = "compact", valueFormat = null) {
   let suffix = ''
   if (valueFormat === '%') {
     n = (n - 1) * 100
     suffix = ' %'
   }
-  let f = new Intl.NumberFormat('en-US', {notation: notation, minimumFractionDigits: 2})
+  let f = NOTATIONS[notation]
   return f.format(n) + suffix
 }
