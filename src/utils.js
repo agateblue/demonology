8925758ca7c09b8e3210378fb2d509417fdcc291
Agate <me@agate.blue>
2022-05-20 11:02:28 +0200

    Attempt to always have digits in bigger numbers

--- src/utils.js
+++ src/utils.js
@@ -5,11 +5,14 @@ export function renderMarkdown (source) {
 }
 
 const NOTATIONS = {
-  default: new Intl.NumberFormat('en-US', {notation: 'compact', minimumFractionDigits: 0, maximumFractionDigits: 2}),
-  compact: new Intl.NumberFormat('en-US', {notation: 'compact', minimumFractionDigits: 0, maximumFractionDigits: 2}),
-  scientific: new Intl.NumberFormat('en-US', {notation: 'scientific', minimumFractionDigits: 0, maximumFractionDigits: 2}),
+  default: new Intl.NumberFormat('en-US', {notation: 'scientific', minimumFractionDigits: 2, maximumFractionDigits: 2}),
+  compact: new Intl.NumberFormat('en-US', {notation: 'compact', minimumFractionDigits: 2, maximumFractionDigits: 2}),
+  scientific: new Intl.NumberFormat('en-US', {notation: 'scientific', minimumFractionDigits: 2, maximumFractionDigits: 2}),
 }
 export function formatNumber (n, notation = 'default', valueFormat = null) {
+  if (n <= 10000) {
+    return n
+  }
   let suffix = ''
   if (valueFormat === '%') {
     n = (n - 1) * 100
