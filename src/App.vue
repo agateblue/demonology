ee806c2f14b73ab64571a587f46b8deeaae3341b
Agate <me@agate.blue>
2022-05-21 05:28:31 +0200

    Added safeguard to avoid mutiple tabs

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,66 @@
 <template>
-  <nav
-    class="text--center my-4"
-  >
-    <router-link class="discrete" to="/">Netherworld</router-link> ·
-    <!-- <router-link class="discrete" to="/">Lore</router-link> · -->
-    <!-- <router-link class="discrete" to="/">Lord</router-link> · -->
-    <router-link class="discrete" to="/statistics">Statistics</router-link> ·
-    <router-link class="discrete" to="/settings">Settings</router-link> ·
-    <router-link class="discrete" to="/about">About</router-link>
-  </nav>
-  <div class="text--center py-4">
-    <h1 class="mt-4">
-      <template v-if="!$store.state.current.name">Who am I?</template>
-      <template v-else>
-        I am {{ $store.getters['values']('names.current').name }}, {{ $store.getters['values']('names.current').title }} 
-      </template>
+  <section v-if="duplicateTab" class="mt-4 tiny">
+    <h1>
+      A Demonology tab is already opened
     </h1>
-    <div class="align-items--center justify-content--center">
-      <fire class="mr-4"></fire>
-      <div class="py-2" v-if="$store.getters['values']('prompts.current')">
-        <p v-for="(row, idx) in $store.getters['values']('prompts.current').text" :key="idx">
-          <i>
-            {{ row }}
-          </i>
-        </p>
+    <p>Another Demonology tab is open, please close this one and use the other one to avoid bugs and glitches.</p>
+  </section>
+  <template v-else>
+    <nav
+      class="text--center my-4"
+    >
+      <router-link class="discrete" to="/">Netherworld</router-link> ·
+      <!-- <router-link class="discrete" to="/">Lore</router-link> · -->
+      <!-- <router-link class="discrete" to="/">Lord</router-link> · -->
+      <router-link class="discrete" to="/statistics">Statistics</router-link> ·
+      <router-link class="discrete" to="/settings">Settings</router-link> ·
+      <router-link class="discrete" to="/about">About</router-link>
+    </nav>
+    <div class="text--center py-4">
+      <h1 class="mt-4">
+        <template v-if="!$store.state.current.name">Who am I?</template>
+        <template v-else>
+          I am {{ $store.getters['values']('names.current').name }}, {{ $store.getters['values']('names.current').title }} 
+        </template>
+      </h1>
+      <div class="align-items--center justify-content--center">
+        <fire class="mr-4"></fire>
+        <div class="py-2" v-if="$store.getters['values']('prompts.current')">
+          <p v-for="(row, idx) in $store.getters['values']('prompts.current').text" :key="idx">
+            <i>
+              {{ row }}
+            </i>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="align-items--center justify-content--center mb-4 text--2">
-    <number-badge
-      class="ml-4 float--right"
-      unit="souls"
-      :value="parseInt($store.state.current.souls)"
-      v-if="$store.state.awakening.souls > 0"
-    > Souls</number-badge>
-    <number-badge
-      class="ml-4 float--right"
-      unit="preys"
-      :value="parseInt($store.state.current.preys)"
-      v-if="$store.getters['values']('preys.enabled')"
-    > Preys</number-badge>
-    <number-badge
-      class="ml-4 float--right"
-      unit="pain"
-      :value="parseInt($store.state.current.pain)"
-      v-if="$store.getters['values']('pain.enabled')"
-    > Pain</number-badge>
-  </div>
-  <router-view/>
-  <div style="position: fixed; bottom: 1em; right: 1em" v-if="$store.state.settings.debug">
-    <button @click.prevent="$store.commit('reset')">
-      Reset
-    </button>
-  </div>
+    <div class="align-items--center justify-content--center mb-4 text--2">
+      <number-badge
+        class="ml-4 float--right"
+        unit="souls"
+        :value="parseInt($store.state.current.souls)"
+        v-if="$store.state.awakening.souls > 0"
+      > Souls</number-badge>
+      <number-badge
+        class="ml-4 float--right"
+        unit="preys"
+        :value="parseInt($store.state.current.preys)"
+        v-if="$store.getters['values']('preys.enabled')"
+      > Preys</number-badge>
+      <number-badge
+        class="ml-4 float--right"
+        unit="pain"
+        :value="parseInt($store.state.current.pain)"
+        v-if="$store.getters['values']('pain.enabled')"
+      > Pain</number-badge>
+    </div>
+    <router-view/>
+    <div style="position: fixed; bottom: 1em; right: 1em" v-if="$store.state.settings.debug">
+      <button @click.prevent="$store.commit('reset')">
+        Reset
+      </button>
+    </div>
+  </template>
 </template>
 <script>
 import {bind, unbind} from '@/hotkeys'
@@ -68,6 +76,7 @@ export default {
   data () {
     return {
       loop: null,
+      duplicateTab: false,
       hotkeys: [
         {key: '&, 1', handler: () => { this.$router.push('/')}},
         {key: 'é, 2', handler: () => { this.$router.push('/statistics')}},
@@ -78,6 +87,22 @@ export default {
     }
   },
   mounted () {
+    // ensure multiple tabs of the game don't run in parallel
+    const bc = new BroadcastChannel("demonology")
+
+    bc.onmessage = (event) => {
+      if (event.data === `started`) {
+        bc.postMessage(`already opened`)
+      }
+      if (event.data === `already opened`) {
+        this.duplicateTab = true
+      }
+    }
+
+    bc.postMessage(`started`)
+    if (this.duplicateTab) {
+      return
+    }
     bind(this.hotkeys)
     let interval = 300
     this.loop = window.setInterval(() => {
