843cd22c61fa13a57b42ab1981731fcf8f90f537
Agate <me@agate.blue>
2022-05-23 18:50:57 +0200

    See #4: added tooltip component

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="tooltip" role="tooltip">
+    <div class="tooltip--content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,64 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router)
+
+function positionTooltip(parent, tooltip) {
+  const screenPadding = 16
+  const maxWidth = window.outerWidth - screenPadding * 2
+  const width = Math.min(250, maxWidth)
+  const parentRect = parent.getBoundingClientRect()
+
+  tooltip.style.width = `${width}px`
+
+  let widthDiff = parentRect.width - width
+  let rightOverflow = parentRect.right - widthDiff + screenPadding - window.outerWidth
+  if (rightOverflow > 0) {
+    tooltip.style.right = `0px`
+
+  } else if (parentRect.x + widthDiff / 2 <= 0) {
+    tooltip.style.left = `0px`
+  } else {
+    tooltip.style.left = `${parseInt(widthDiff / 2)}px`
+  }
+}
+
+function showTooltip (id) {
+  return (event) => {
+    let tooltip = document.getElementById(id)
+    if (!event.target.contains(tooltip) && !tooltip.contains(event.target)) {
+      event.target.appendChild(tooltip)
+    }
+    if (tooltip && !tooltip.classList.contains('visible')) {
+      tooltip.classList.add('visible')
+      positionTooltip(event.target, tooltip)
+    }
+  }
+}
+function hideTooltip () {
+  return () => {
+    let tooltips = document.querySelectorAll('[role=tooltip]')
+    for (const tooltip of tooltips) {
+      tooltip.classList.remove('visible')
+    }
+  }
+}
+app.directive('tooltip', (el, binding) => {
+  let tooltipId = binding.value
+  el.setAttribute('aria-describedby', tooltipId)
+  el.setAttribute('tabindex', 0)
+  el.classList.add('tooltip--parent')
+  el.addEventListener('focus', showTooltip(tooltipId))
+  el.addEventListener('blur', hideTooltip())
+  el.addEventListener('mouseover', showTooltip(tooltipId))
+  el.addEventListener('mouseleave', hideTooltip())
+  el.addEventListener('click', showTooltip(tooltipId))
+  document.addEventListener('click', function (event) {
+    // hide tooltip when a click happens outside
+    const withinBoundaries = event.composedPath().includes(event.target)
+    if (!withinBoundaries) {
+      hideTooltip()(event)
+    }
+  })
+})
+app.mount('#app')
